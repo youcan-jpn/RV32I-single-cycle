ae1c1ca874eafc8cc2f4793a5334f6e208db9964
yuta ono <youcana40627@gmail.com>
2023-07-27 10:04:36 +0900

    fix: immediate extension

--- extend.sv
+++ extend.sv
@@ -8,7 +8,7 @@ module extend(input  logic [31:7] instr,
             // S-type
             2'b01:   immext = {{20{instr[31]}}, instr[31:25], instr[11:7]};
             // B-type (branches)
-            2'b10:   immext = {{20{instr[31]}}, instr[11:8], 1'b0};
+            2'b10:   immext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
             // J-type (jal)
             2'b11:   immext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
             default: immext = 32'bx; // undefined
